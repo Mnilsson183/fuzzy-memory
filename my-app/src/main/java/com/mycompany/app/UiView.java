e302496684bcff573084553f21e45440ada7d524
Morgan Nilsson <morgan.nilsson7111@icloud.com>
2023-10-02 09:40:01 -0600

    toggle switch

--- my-app/src/main/java/com/mycompany/app/UiView.java
+++ my-app/src/main/java/com/mycompany/app/UiView.java
@@ -22,13 +22,14 @@ public class UiView extends JFrame{
     private JTextField m_reps = new JTextField(5);
     private JTextField m_weight = new JTextField(20);
     private JButton    m_submit = new JButton("Submit");
+    private ToggleSwitch m_Switch = new ToggleSwitch();
 
 
     //================================ constructor
     UiView(){
 
         m_logic = new AppModel();
-        
+
     }
 
 }
